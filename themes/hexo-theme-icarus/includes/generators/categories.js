576b052119744ba1338079b9e618f85128bdbd57
Gooogle <jasonandy@hotmail.com>
2019-01-17 09:58:53 +0800

    Update blog sourceCode

---
+++ themes/hexo-theme-icarus/includes/generators/categories.js
@@ -0,0 +1,11 @@
+module.exports = function (hexo) {
+    hexo.extend.generator.register('categories', function (locals) {
+        return {
+            path: 'categories/',
+            layout: ['categories'],
+            data: Object.assign({}, locals, {
+                __categories: true
+            })
+        };
+    });
+}
---
+++ themes/hexo-theme-icarus/includes/generators/tags.js
@@ -0,0 +1,11 @@
+module.exports = function (hexo) {
+    hexo.extend.generator.register('tags', function (locals) {
+        return {
+            path: 'tags/',
+            layout: ['tags'],
+            data: Object.assign({}, locals, {
+                __tags: true
+            })
+        };
+    });
+}
